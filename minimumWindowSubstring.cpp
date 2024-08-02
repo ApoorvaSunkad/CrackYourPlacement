6f01010059c131e48b5e6530e601cf671fd9c4b0
ApoorvaSunkad <apoorvasunkad1999@gmail.com>
2024-08-02 15:46:29 +0530

    Minimum window substring

---
+++ minimumWindowSubstring.cpp
@@ -0,0 +1,87 @@
+/*
+Given two strings s and t of lengths m and n respectively, 
+return the minimum window 
+substring of s such that every character in t (including duplicates) is 
+included in the window. If there is no such substring, return the empty 
+string "".
+
+The testcases will be generated such that the answer is unique.
+
+ 
+
+Example 1:
+
+Input: s = "ADOBECODEBANC", t = "ABC"
+Output: "BANC"
+Explanation: The minimum window substring "BANC" includes 'A', 'B', 
+and 'C' from string t.
+
+Example 2:
+
+Input: s = "a", t = "a"
+Output: "a"
+Explanation: The entire string s is the minimum window.
+Example 3:
+
+Input: s = "a", t = "aa"
+Output: ""
+Explanation: Both 'a's from t must be included in the window.
+Since the largest window of s only has one 'a', return empty string.
+ 
+
+Constraints:
+
+m == s.length
+n == t.length
+1 <= m, n <= 105
+s and t consist of uppercase and lowercase English letters.
+ 
+
+Follow up: Could you find an algorithm that runs in O(m + n) time?
+
+*/
+#include<iostream>
+#include<string>
+#include<map>
+using namespace std;
+//T.C = O(M+N)
+//S.C = O(N)
+
+string minWindow(string s, string t) {
+        int n = s.length();
+        map<char, int> mp;
+        
+        for(char &ch : t) {
+            mp[ch]++;
+        }
+        
+        int requiredCount = t.length();
+        int i = 0, j  = 0;
+        int minStart  = 0;
+        int minWindow = INT_MAX;
+        while(j < n) {
+            char ch_j = s[j];
+            if(mp[ch_j] > 0)
+                requiredCount--;
+            
+            mp[ch_j]--;
+            
+            while(requiredCount == 0) { //try to shrink the window
+                if(minWindow > j-i+1) {
+                    minWindow = j-i+1;
+                    minStart  = i;
+                }
+                
+                char ch_i = s[i];
+                mp[ch_i]++;
+                if(mp[ch_i] > 0)
+                    requiredCount++;
+                i++;
+            }
+            
+            j++; //Don't ever forget this :-)
+        }
+        
+        return minWindow == INT_MAX ? "" : s.substr(minStart, minWindow);
+
+    }
