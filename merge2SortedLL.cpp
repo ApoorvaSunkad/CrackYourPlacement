139cc1fe40e98cb78bea635ac9ca06f0d38759ff
ApoorvaSunkad <apoorvasunkad1999@gmail.com>
2024-08-21 06:05:46 +0530

    merge 2 sorted linked lists

---
+++ merge2SortedLL.cpp
@@ -0,0 +1,61 @@
+/*
+
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by 
+splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+Example 1:
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+
+*/
+
+#include<iostream>
+using namespace std;
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* t1 = list1;
+        ListNode* t2 = list2;
+
+        ListNode* dNode = new ListNode(-1);
+        ListNode* temp = dNode;
+
+        while(t1 != NULL && t2 != NULL){
+            if(t1->val <= t2->val){
+                temp->next = t1;
+                temp = t1;
+                t1 = t1->next;
+            }else{
+                temp->next = t2;
+                temp = t2;
+                t2 = t2->next;
+            }
+        }
+        if(t1) temp->next = t1;
+        else temp->next = t2;
+        return dNode->next;
+    }
